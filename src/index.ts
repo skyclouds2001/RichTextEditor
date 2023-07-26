1e9634502919dac0f196c7b49b0155efa50307f1
skyclouds2001 <skyclouds2001@163.com>
2023-07-26 19:38:41 +0800

    feat: support for insert image

--- src/index.ts
+++ src/index.ts
@@ -118,6 +118,11 @@ class TextEditor {
       icon: `url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="m19.25 16.45l-1.5-1.55q1-.275 1.625-1.063T20 12q0-1.25-.875-2.125T17 9h-3q-.425 0-.713-.288T13 8q0-.425.288-.713T14 7h3q2.075 0 3.538 1.463T22 12q0 1.425-.738 2.625T19.25 16.45Zm-3.625-3.675L13.85 11H15q.425 0 .713.288T16 12q0 .25-.1.45t-.275.325ZM20.5 21.9q-.275.275-.7.275t-.7-.275l-17-17q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l17 17q.275.275.275.7t-.275.7ZM7 17q-2.075 0-3.538-1.463T2 12q0-1.725 1.05-3.075t2.7-1.775L7.6 9H7q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15h3q.425 0 .713.288T11 16q0 .425-.288.713T10 17H7Zm2-4q-.425 0-.713-.288T8 12q0-.425.288-.713T9 11h.625l1.975 2H9Z"%2F%3E%3C%2Fsvg%3E')`,
       cmd: 'unlink',
     },
+    {
+      name: 'material-symbols:imagesmode-outline-rounded',
+      icon: `url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14Zm0 0V5v14Zm2-2h10q.3 0 .45-.275t-.05-.525l-2.75-3.675q-.15-.2-.4-.2t-.4.2L11.25 16L9.4 13.525q-.15-.2-.4-.2t-.4.2l-2 2.675q-.2.25-.05.525T7 17Zm1.5-7q.625 0 1.063-.438T10 8.5q0-.625-.438-1.063T8.5 7q-.625 0-1.063.438T7 8.5q0 .625.438 1.063T8.5 10Z"%2F%3E%3C%2Fsvg%3E')`,
+      cmd: 'insertImage',
+    },
   ]
 
   readonly isEditor: boolean
@@ -379,6 +384,10 @@ class TextEditor {
           break
         case 'unlink':
           this.#container.contentDocument!.execCommand('unlink')
+          break
+        case 'insertImage':
+          const image = window.prompt('Please input the source of image')
+          if (typeof image === 'string') this.#container.contentDocument!.execCommand('insertImage', false, image)
           break
       }
     }
