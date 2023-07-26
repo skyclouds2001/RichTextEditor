8ab804e9a856ac80fbc78ce279dfd530d11fd5c9
skyclouds2001 <skyclouds2001@163.com>
2023-07-26 10:40:49 +0800

    feat: support list

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,16 @@ class TextEditor {
       cmd: 'subscript',
     },
     {
+      name: 'material-symbols:format-list-bulleted-rounded',
+      icon: `url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M10 19q-.425 0-.713-.288T9 18q0-.425.288-.713T10 17h10q.425 0 .713.288T21 18q0 .425-.288.713T20 19H10Zm0-6q-.425 0-.713-.288T9 12q0-.425.288-.713T10 11h10q.425 0 .713.288T21 12q0 .425-.288.713T20 13H10Zm0-6q-.425 0-.713-.288T9 6q0-.425.288-.713T10 5h10q.425 0 .713.288T21 6q0 .425-.288.713T20 7H10ZM5 20q-.825 0-1.413-.588T3 18q0-.825.588-1.413T5 16q.825 0 1.413.588T7 18q0 .825-.588 1.413T5 20Zm0-6q-.825 0-1.413-.588T3 12q0-.825.588-1.413T5 10q.825 0 1.413.588T7 12q0 .825-.588 1.413T5 14Zm0-6q-.825 0-1.413-.588T3 6q0-.825.588-1.413T5 4q.825 0 1.413.588T7 6q0 .825-.588 1.413T5 8Z"%2F%3E%3C%2Fsvg%3E')`,
+      cmd: 'insertUnorderedList',
+    },
+    {
+      name: 'material-symbols:format-list-numbered-rounded',
+      icon: `url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M3.75 22q-.325 0-.537-.213T3 21.25q0-.325.213-.537t.537-.213H5.5v-.75h-.75q-.325 0-.537-.213T4 19q0-.325.213-.537t.537-.213h.75v-.75H3.75q-.325 0-.537-.213T3 16.75q0-.325.213-.537T3.75 16H6q.425 0 .713.288T7 17v1q0 .425-.288.713T6 19q.425 0 .713.288T7 20v1q0 .425-.288.713T6 22H3.75Zm0-7q-.325 0-.537-.213T3 14.25v-2q0-.425.288-.713T4 11.25h1.5v-.75H3.75q-.325 0-.537-.213T3 9.75q0-.325.213-.537T3.75 9H6q.425 0 .713.288T7 10v1.75q0 .425-.288.713T6 12.75H4.5v.75h1.75q.325 0 .537.213T7 14.25q0 .325-.213.537T6.25 15h-2.5Zm1.5-7q-.325 0-.537-.213T4.5 7.25V3.5h-.75q-.325 0-.537-.213T3 2.75q0-.325.213-.537T3.75 2h1.5q.325 0 .537.213T6 2.75v4.5q0 .325-.213.537T5.25 8ZM10 19q-.425 0-.713-.288T9 18q0-.425.288-.713T10 17h10q.425 0 .713.288T21 18q0 .425-.288.713T20 19H10Zm0-6q-.425 0-.713-.288T9 12q0-.425.288-.713T10 11h10q.425 0 .713.288T21 12q0 .425-.288.713T20 13H10Zm0-6q-.425 0-.713-.288T9 6q0-.425.288-.713T10 5h10q.425 0 .713.288T21 6q0 .425-.288.713T20 7H10Z"%2F%3E%3C%2Fsvg%3E')`,
+      cmd: 'insertOrderedList',
+    },
+    {
       name: 'material-symbols:link-rounded',
       icon: `url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M7 17q-2.075 0-3.538-1.463T2 12q0-2.075 1.463-3.538T7 7h3q.425 0 .713.288T11 8q0 .425-.288.713T10 9H7q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15h3q.425 0 .713.288T11 16q0 .425-.288.713T10 17H7Zm2-4q-.425 0-.713-.288T8 12q0-.425.288-.713T9 11h6q.425 0 .713.288T16 12q0 .425-.288.713T15 13H9Zm5 4q-.425 0-.713-.288T13 16q0-.425.288-.713T14 15h3q1.25 0 2.125-.875T20 12q0-1.25-.875-2.125T17 9h-3q-.425 0-.713-.288T13 8q0-.425.288-.713T14 7h3q2.075 0 3.538 1.463T22 12q0 2.075-1.463 3.538T17 17h-3Z"%2F%3E%3C%2Fsvg%3E')`,
       cmd: 'createLink',
@@ -249,6 +259,12 @@ class TextEditor {
           break
         case 'subscript':
           this.#container.contentDocument!.execCommand('subscript')
+          break
+        case 'insertOrderedList':
+          this.#container.contentDocument!.execCommand('insertOrderedList')
+          break
+        case 'insertUnorderedList':
+          this.#container.contentDocument!.execCommand('insertUnorderedList')
           break
         case 'createLink':
           const link = window.prompt('Please input the link')
